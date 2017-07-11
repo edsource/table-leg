a411f2f67edadd385ecfb60e7f4f291e620dd2b1
Yuxuan Xie <yxx281@miami.edu>
2017-07-10 17:04:35 -0700

    hide gridlines and X-axis tick labels, make scale 100%
    hide gridlines for both X & Y,  hide X-axis tick labels, make Xscale
    100%

--- js/main.js
+++ js/main.js
@@ -43,13 +43,21 @@ jQuery(document).ready(function($){
 		    },
 		    options: {
 		        scales: {
-		            xAxes: [{
-		                ticks: {
-		                    beginAtZero:true
-		                }
-		            }],
+                    xAxes:[{
+                        gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                     display: false,
+                    ticks: {
+                        beginAtZero:true,
+                        max:100
+                    }
+                    }],
 		            yAxes: [{
-		            	stacked: true,
+                        stacked: true,
+                        gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
 		                ticks: {
 		                    beginAtZero:true
 		                }
